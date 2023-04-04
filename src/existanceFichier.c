e08fa9edad2712ad24693b26f922453674d4cbd0
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2023-04-04 20:05:51 +0200

    vérification de l'existance d'un fichier

--- src/existanceFichier.c
+++ src/existanceFichier.c
@@ -5,7 +5,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
+/**
+ * @brief fonction qui permet de savoir si le nombre d'arguments est correcte
+ *
+ * @param nb_arguments nombre d'arguments dans le main
+ */
 void validation_arguments(int nb_arguments){
     //le nombre d'arguments doit être de 1
     if(nb_arguments != 2){
@@ -14,10 +20,23 @@ void validation_arguments(int nb_arguments){
     }
 }
 
+/**
+ * @brief fonction qui permet de savoir si le fichier existe
+ *
+ * @param nom_fichier nom de fihier à vérifier
+ */
+void fichier_existe(char * nom_fichier){
+    if(access(nom_fichier, F_OK ) != -1){
+        perror("le fichier n'existe pas !!!");
+        exit(-1);
+    }
+}
+
 
 int main(int argc, char** argv){
 
     validation_arguments(argc);
+    fichier_existe(argv[1]);
 
     return EXIT_SUCCESS;
 }
