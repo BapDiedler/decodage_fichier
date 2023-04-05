cd8a126319f162a68147b3b0a6d3bbd092c001a0
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2023-04-05 19:50:34 +0200

    milieu de la programmation.

--- src/decalageMessage.c
+++ src/decalageMessage.c
@@ -12,35 +12,46 @@ int main(int argc, char** argv){
     //vérification du nombre d'arguments
     int lecture = atoi(argv[2]);
     int ecriture = atoi(argv[1]);
-    char* mot_test = "TEST";
+    char* mot_test = argv[3];
     char tableau_test[100];
 
+    printf("MESSAGE : ");
 
     char buffer;
     close(ecriture) ;
     int i=0;
     int trouve = 1;
+    int new_cara;
     while(read(lecture,&buffer, sizeof(char))){
-        if(buffer=='B'){
+        new_cara = (buffer-65-atoi(argv[4]))%27;
+        if(new_cara+65=='@'){
             printf(" ");
+            fflush(stdout);
             tableau_test[i]= '\0';
-            if(strcmp(tableau_test,mot_test)!=0 && trouve){
+            if(strcmp(tableau_test,mot_test)==0 && trouve==1){
                 trouve=0;
-                i=0;
             }
-        }else if(buffer >= 'A' && buffer <= 'Z'){
-            printf("%c", buffer - 2);
-            tableau_test[i]= buffer - 2;
+            i=-1;
+            printf("%c",tableau_test[i]);
+            fflush(stdout);
+
+        }else if(new_cara+65 >= 'A' && new_cara+65 <= 'Z'){
+            printf("%c", (char)(new_cara+65));
+            fflush(stdout);
+            tableau_test[i] = (char)(new_cara+65);
         }else{
             printf("%c", buffer);
+            fflush(stdout);
             tableau_test[i]= buffer;
         }
         i++;
     }
-    if(!trouve){
-        printf("\n le message est trouvé\n");
-    }
     close(lecture) ;
-
-    return EXIT_SUCCESS;
+    printf("\n");
+    fflush(stdout);
+    if(trouve==1){
+        return 0;
+    }
+    printf("le mot est trouvé\n");
+    return atoi(argv[4]);
 }
--- src/decrypteMessage.c
+++ src/decrypteMessage.c
@@ -1,12 +1,13 @@
 /**
 * @brief le programme permet de tester si un fichier donné en paramètre éxiste ou non
- * @author baptistedie
+* @author baptistedie
 */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 
 
 /**
@@ -16,7 +17,7 @@
  */
 void validation_arguments(int nb_arguments){
     //le nombre d'arguments doit être de 1
-    if(nb_arguments != 2){
+    if(nb_arguments != 4){
         perror("le nombre d'arguments doit être de 1");
         exit(-1);
     }
@@ -24,9 +25,9 @@ void validation_arguments(int nb_arguments){
 
 
 /**
- *
+ * fonction qui permet de créer un processus
  */
-void creation_processus(int lecture,int ecriture){
+void creation_processus(int lecture,int ecriture, char* mot, char* decalage){
     pid_t res;
     char valLecture[5], valEcriture[5];
     sprintf(valEcriture,"%d",ecriture);
@@ -37,7 +38,7 @@ void creation_processus(int lecture,int ecriture){
             perror("erreur dans la créatiob de processus");
             exit(1);
         case (pid_t) 0 ://programme fils
-            execl("./decalageMessage","decalageMessage",valEcriture,valLecture,NULL);
+            execl("./decalageMessage","decalageMessage",valEcriture,valLecture,mot,decalage,NULL);
             fflush(stdout);
             break;
         default ://programme père
@@ -45,21 +46,17 @@ void creation_processus(int lecture,int ecriture){
     }
 }
 
-
-/**
- * @biref méthode qui permet de vérifier si le fichier est valide et le manipule
- *
- * @param nom_fichier nom de fichier à tester
- */
-void fichier_manipulation(char* nom_fichier){
-
+int ouverture_fichier(char* nom_fichier){
     //ouverture du fichier
     int fp = open(nom_fichier,O_RDONLY);
     if (fp == -1) {
         perror("Erreur lors de l'ouverture du fichier");
         exit(-1);
     }
+    return fp;
+}
 
+void test_fichier_crypt(int fp){
     //test fichier crypté
     char buffer[2];
     if(read(fp,&buffer, 2*sizeof(char))){
@@ -69,61 +66,91 @@ void fichier_manipulation(char* nom_fichier){
             exit(-1);
         }
     }
+}
 
-    //test valeurs fichier
+void test_donnees_fichier(int fp,int* bufferInt1, int* bufferInt2){
     int bufferInt[2];
+    //test valeurs fichier
     if(read(fp,&bufferInt,2* sizeof(int))){
-        if(!(bufferInt[0]>=0 && bufferInt[0]<=lseek(fp,0,SEEK_END))){
+        if(!(bufferInt[0]>=0 && bufferInt[0] < lseek(fp, 0, SEEK_END))){
             perror("la taille du fichier n'est pas correcte");
             exit(-1);
+        }else{
+            *bufferInt1 = bufferInt[0];
         }
-        if(!(bufferInt[1]>0 && bufferInt[1]<lseek(fp,0,SEEK_END))){
+        if(!(bufferInt[1]>=0 && bufferInt[1] < lseek(fp, 0, SEEK_END) && bufferInt[0] > bufferInt[1])){
             perror("les valeurs du fichier ne sont pas correctes");
             exit(-1);
+        }else{
+            *bufferInt2 = bufferInt[1];
         }
     }
+}
 
-    int tube[2];
-    char cara;
-
-    if (pipe(tube) == -1){ /*création d'un tube*/
-        perror("création impossible du tube");fflush(stdout);
-        exit(1);
+void recuperation_donnees_fils(char * decalage, char * nom_fichier){
+    int status;
+    wait(&status); // Attendre la fin de l'exécution du processus fils
+    fflush(stdout);
+    if (WIFEXITED(status)) {
+        if(WEXITSTATUS(status)==atoi(decalage)){
+            printf("FICHIER : %s\n",nom_fichier);
+            printf("DECALAGE : %s\n",decalage);
+        }
     }
-
-    int ecriture = tube[1];
-    int lecture = tube[0];
-
-    creation_processus(lecture,ecriture);
+}
 
 
+void lecture_message(int lecture, int ecriture, int fp, int* bufferInt){
+
+    char cara;
     close(lecture) ;
-
     //lecture du message
-    lseek(fp,bufferInt[1]+2* sizeof(int)+2* sizeof(char),SEEK_SET);
-    for(int i=0; i<bufferInt[0];i++){
+    lseek(fp, (__off_t) (bufferInt[1] + 2 * sizeof(int) + 2 * sizeof(char)), SEEK_SET);
+    for(int i=0;  i < bufferInt[0]; i++){
         if(!read(fp,&cara, sizeof(char))){
             perror("erreur dans la lecture du fichier");
             exit(-1);
         }
         /* écriture (buffer est une suite d'un ou plusieurs octets ici de taille char)*/
-        write(tube[1],&cara, sizeof(char)) ;
+        write(ecriture,&cara, sizeof(char)) ;
     }
-
     /* fin écriture */
     close(ecriture) ;
+}
 
 
+/**
+ * @biref méthode qui permet de vérifier si le fichier est valide et le manipule
+ *
+ * @param nom_fichier nom de fichier à tester
+ */
+void fichier_manipulation(char* nom_fichier, char* mot, char* decalage){
+    int fp = ouverture_fichier(nom_fichier);
+    test_fichier_crypt(fp);
+    int bufferInt[2];
+    test_donnees_fichier(fp,&bufferInt[0],&bufferInt[1]);
+    int tube[2];
+    if (pipe(tube) == -1){ /*création d'un tube*/
+        perror("création impossible du tube");fflush(stdout);
+        exit(1);
+    }
+    int ecriture = tube[1];
+    int lecture = tube[0];
+    creation_processus(lecture,ecriture,mot,decalage);
+    lecture_message(lecture,ecriture,fp,bufferInt);
+    recuperation_donnees_fils(decalage,nom_fichier);
     close(fp);
 }
 
 
 int main(int argc, char** argv){
 
+    validation_arguments(argc);
+
     char* nom_fichier = argv[1];
 
-    validation_arguments(argc);
-    fichier_manipulation(nom_fichier);
+
+    fichier_manipulation(nom_fichier,argv[2],argv[3]);
 
     return EXIT_SUCCESS;
 }
